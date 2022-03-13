86f62103fefd97ab4695e43063a35144bd6ee846
Papp Márk <pmarkx@stud.uni-obuda.hu>
2022-03-13 14:04:22 +0100

    Add project files.

---
+++ GUI_20212202_DCQEB4/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI_20212202_DCQEB4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
