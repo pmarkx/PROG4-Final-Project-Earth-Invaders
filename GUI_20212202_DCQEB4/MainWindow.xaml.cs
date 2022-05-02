f12917e9bebfa002938a12d8bce02361da982cc4
Mark Papp <Mark.Papp@ibm.com>
2022-05-02 19:52:44 +0200

    autoplay fixed

--- GUI_20212202_DCQEB4/MainWindow.xaml.cs
+++ GUI_20212202_DCQEB4/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace GUI_20212202_DCQEB4
         public MainWindow()
         {
             InitializeComponent();
+            myMediaElement.Play();
             GameLogic logic = new GameLogic();
             display.SetupModel(logic);
             controller = new GameController(logic);
