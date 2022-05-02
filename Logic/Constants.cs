b84714b1fa83c37871db201f86739ff7c867b429
Mark Papp <Mark.Papp@ibm.com>
2022-05-02 22:01:55 +0200

    minor adjustments in dificulty

--- Logic/Constants.cs
+++ Logic/Constants.cs
@@ -15,18 +15,18 @@ namespace Logic
             control.BulletMoveInterval = new TimeSpan(0, 0, 0, 0, 200);
             control.ShootingBetweenInterval = new TimeSpan(0, 0, 0, 2);*/
 
-        public static TimeSpan GameTickInterval = new TimeSpan(0, 0, 0, 0, 99);
-        public static TimeSpan EnemyMovementInterval = new TimeSpan(0, 0, 0, 0, 301);
-        public static TimeSpan EnemySpawnInterval = new TimeSpan(0, 0, 0, 0,399);
-        public static TimeSpan BulletMoveInterval = new TimeSpan(0, 0, 0, 0, 199);
-        public static TimeSpan ShootingBetweenInterval = new TimeSpan(0, 0, 0, 2);
+        public static TimeSpan GameTickInterval = new TimeSpan(0, 0, 0, 0, 1);
+        public static TimeSpan EnemyMovementInterval = new TimeSpan(0, 0, 0, 0, 159);
+        public static TimeSpan EnemySpawnInterval = new TimeSpan(0, 0, 0, 0,89);
+        public static TimeSpan BulletMoveInterval = new TimeSpan(0, 0, 0, 0, 59);
+        public static TimeSpan ShootingBetweenInterval = new TimeSpan(0, 0, 0, 1);
         public static TimeSpan LifeSpawnInterval = new TimeSpan(0, 0, 0, 10);
         public static TimeSpan AmmoSpawnInterval = new TimeSpan(0, 0, 0, 4);
 
         public static TimeSpan UIRefreshInterval = new TimeSpan(3);
         public static long DefaultScore = 0;
-        public static int DefaultLifes = 3;
-        public static int DefaultAmmo = 3;
+        public static int DefaultLifes = 5;
+        public static int DefaultAmmo = 10;
 
         public enum Directions
         {
