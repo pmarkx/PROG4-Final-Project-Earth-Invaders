e196be4d4bff54fbe8136b780d93f52fe246b593
Mark Papp <Mark.Papp@ibm.com>
2022-05-09 19:27:52 +0200

    minor gameplay change

--- Logic/Models/Mine.cs
+++ Logic/Models/Mine.cs
@@ -12,7 +12,7 @@ namespace Logic.Models
         {
         }
 
-        public override int Priority => 1;
+        public override int Priority => 3;
 
         public override void Collided(IEnumerable<GameObject> collidedWith)
         {
